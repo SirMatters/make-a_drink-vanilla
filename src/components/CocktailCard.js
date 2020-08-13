950d0bb3d17be88f8f980c6a82dd2ca3c484028e
SirMatters <perov.krll@gmail.com>
2020-08-13 21:16:24 +0300

    Add image display to CocktailCard

--- src/components/CocktailCard.js
+++ src/components/CocktailCard.js
@@ -5,10 +5,18 @@ const CocktailCard = ({ cocktail, history }) => {
   const onClick = () => {
     history.push(`/cocktails/${cocktail.id}`);
   };
+
+  const style = {
+    backgroundImage: `url(${cocktail.image})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    minHeight: '100%',
+  };
   return (
     <div className='cocktail-card' onClick={onClick}>
       <div className='cocktail-card__info'>
-        <div className='cocktail-card__img'>{cocktail.name} img</div>
+        <div className='cocktail-card__img' style={style}></div>
         <div className='cocktail-card__raiting'>{cocktail.raiting}</div>
       </div>
       <div className='cocktail-card__name'>{cocktail.name}</div>
