4117e4ca71c41713e1ac4b773c0af1ea9f07ee3f
SirMatters <perov.krll@gmail.com>
2020-09-08 23:05:49 +0300

    Comment editing API - add _editComment method
    Comment actions and reducer
    - add handleCommentEdit as action handler
    - add reducer handler for edting
    
    <Comment/>
    - add editing state via hooks
    - the state is false by default and triggered by clicking Edit button which is only displayed if authed user is the author
    - isEditing is false if tab or esc key were pressed during edit
    - textarea replaces comment's text when isEditing == true
    - onSubmit handleCommentEdit is dispatched

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,28 +1,67 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { handleCommentDelete } from '../actions/comments';
+import { handleCommentDelete, handleCommentEdit } from '../actions/comments';
 import CommentsList from './CommentsList';
 import NewComment from './NewComment';
 
 const Comment = ({ comment, authedUser, onSelect, isSelected }) => {
   const dispatch = useDispatch();
+  const [isEditing, setEditing] = useState(false);
+  const [text, setText] = useState('');
+
+  useEffect(() => {
+    setText(comment.text);
+  }, [comment, authedUser, onSelect, isSelected]);
 
   const deleteComment = (id) => {
     console.log('click on delete', id);
     dispatch(handleCommentDelete(id));
   };
+
   const onReplySelect = (id) => {
     onSelect(id);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setEditing(false);
+    dispatch(handleCommentEdit({ ...comment, text }));
+  };
+
+  const handleKeyDown = (e) => {
+    const { key } = e;
+    const keys = ['Escape', 'Tab'];
+    if (keys.indexOf(key) > -1) {
+      setEditing(false);
+    }
+  };
+  const onEditClick = () => {
+    setEditing(true);
+  };
+  const onCommentEdit = (e) => {
+    setText(e.target.value);
   };
 
   return (
     <Fragment>
       <div className={'comment'}>
-        {comment.text}
+        {isEditing ? (
+          <form onSubmit={handleSubmit}>
+            <textarea
+              name='text'
+              value={text}
+              onChange={(e) => onCommentEdit(e)}
+              onKeyDown={(e) => handleKeyDown(e)}
+            />
+            <button>Submit</button>
+          </form>
+        ) : (
+          comment.text
+        )}
         {comment.author === authedUser.id && (
           <span onClick={() => deleteComment(comment.id)}> X</span>
         )}
-        {
+        {comment.author !== authedUser.id ? (
           <div
             onClick={() => {
               onReplySelect(comment.id);
@@ -30,7 +69,9 @@ const Comment = ({ comment, authedUser, onSelect, isSelected }) => {
           >
             Reply
           </div>
-        }
+        ) : (
+          <div onClick={onEditClick}>Edit</div>
+        )}
       </div>
       {isSelected && (
         <NewComment
