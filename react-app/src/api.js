e39b389912e016a50f4147477c4897a538c8c8e4
SirMatters <perov.krll@gmail.com>
2020-12-20 15:47:29 +0300

    create test api file for sanity requests

---
+++ react-app/src/api.js
@@ -0,0 +1,22 @@
+import dotenv from 'dotenv'
+const sanityClient = require('@sanity/client');
+
+dotenv.config({path: '.env'});
+const SANITY_TOKEN = process.env.SANITY_TOKEN;
+
+const client = sanityClient({
+  projectId: 'dbheocak',
+  dataset: 'production',
+  token: SANITY_TOKEN,
+  useCdn: true
+})
+
+const query = '*[_type == "cocktail"]';
+client
+  .fetch(query)
+  .then(cocktails => {
+    console.log('All cocktails:')
+    cocktails.forEach(cocktail => {
+      console.log(`${JSON.stringify(cocktail)}`)
+    })
+  })
